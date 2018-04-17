dbd65162a7e16e5394697f16adeb7359bf61e9e3
Rodrigo I. Ávila D <rodrig1a@yahoo.com>
2018-04-17 00:10:41 -0300

    Protected against trailing chars
    When that sweet last step comes, a stuck character could have ruined your results!

--- máxima_pérdida_admisible.c
+++ máxima_pérdida_admisible.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-long double caudal = 0.0;
-long double longitud = 0.0;
-long double headLoss= 0.0;
+long double caudal = 0.0    ;
+long double longitud = 0.0  ;
+long double headLoss= 0.0   ;
 long double viscosidad = 0.0;
-long double densidad = 0.0;
-long double rugosidad = 0.0;
-long double a = 0.0;
-long double reConst= 0.0;
-long double f_i = 4e-1;
-const long double g = 9.81;
+long double densidad = 0.0  ;
+long double rugosidad = 0.0 ;
+long double a = 0.0         ;
+long double reConst= 0.0    ;
+long double f_i = 4e-1      ;
+const long double g = 9.81  ;
 
 long double inline fifthroot (long double radicando){
 	return pow(radicando, 1.0/5.0);
 }
+
+void inline cleanAfter(){             /*This function eats any invalid input stuck */
+	while (getchar() != '\n') {}  /*after retrieving a new friction coeficient.*/
+}				      /*A bad moment for throwing away the results */
 
 int main (){
 	printf("Caudal [m3/s]: ");
@@ -36,7 +40,6 @@ int main (){
 		printf("\tDiámetro: %Lg\n", fifthroot(a*f_i));
 		printf("\tReynolds: %Le\n", reConst / fifthroot(a*f_i));
 		printf("\tRugosidad relativa: %Le\n", rugosidad / fifthroot(a*f_i));
-		/*TO DO: Make a function out of this if, with recursion if it seems appropriate*/
 		if (reConst / fifthroot(a*f_i) < 2000) { /*Si es régimen laminar*/
 			if (f_i - 64 / (reConst / fifthroot(a*f_i)) < 0.0001) {
 				if (64 / (reConst / fifthroot(a*f_i)) - f_i < 0.0001) {
@@ -51,6 +54,7 @@ int main (){
 		else {/*Si no es régimen laminar*/
 			printf("¿f_i+1?: ");
 			scanf ("%Le", &f_i);
+			cleanAfter();
 		}
 		if (f_i <= 0.0) {return 0;}
 	}
